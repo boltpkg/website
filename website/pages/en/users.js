bc0ae4c4fc207cc4af740cf61bd6b0ff5be6fe8d
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-01-15 22:57:58 +1100

    updated gitignore

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
@@ -33,7 +26,7 @@ class Users extends React.Component {
             <div className="logos">{showcase}</div>
             <p>Are you using this project?</p>
             <a
-              href="https://github.com/facebook/docusaurus/edit/master/website/siteConfig.js"
+              href="#"
               className="button">
               Add your company
             </a>
